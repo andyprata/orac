823871aaa031afc789469ea167c2379b11afa89f
Andrew Prata <andyprata@gmail.com>
2021-03-24 13:14:26 +1100

    Turned off predef geo.

--- tools/pyorac/local_defaults.py
+++ tools/pyorac/local_defaults.py
@@ -63,11 +63,11 @@ auxiliaries = {
     'usgs_file'   : '/network/aopp/matin/eodg/shared/Aux_file_CM_SAF_AVHRR_'
                     'GAC_ori_0.05deg.nc',
     # Pre-defined geometry for geostationary imager
-    #'prelsm_file' : '/network/aopp/matin/eodg/shared/orac/data/predef_lsm/'
+    'prelsm_file' : '',#'/network/aopp/matin/eodg/shared/orac/data/predef_lsm/'
                     #'MSG_000E_LSM.nc',  # SEVIRI 0 degrees
                     #'MSG_415E_LSM.nc',  # SEVIRI 41.5 degrees
                     #'AHI_141E_LSM.nc',  # AHI
-    #'pregeo_file' : '/network/aopp/matin/eodg/shared/orac/data/predef_lsm/'
+    'pregeo_file' : '',#'/network/aopp/matin/eodg/shared/orac/data/predef_lsm/'
                     #'MSG_000E_ANGLES.v01.nc',  # SEVIRI 0 degrees
                     #'MSG_415E_ANGLES.nc',  # SEVIRI 41.5 degrees
                     #'AHI_141E_ANGLES.nc',  # AHI
