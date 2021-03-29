7333a16a482931c48460da50952ff0a719ee2460
Andrew Prata <andyprata@gmail.com>
2021-03-29 20:38:49 +1100

    Fixed predef geo

--- tools/pyorac/local_defaults.py
+++ tools/pyorac/local_defaults.py
@@ -64,12 +64,13 @@ auxiliaries = {
                     'GAC_ori_0.05deg.nc',
     # Pre-defined geometry for geostationary imager
     'prelsm_file' : '/network/aopp/matin/eodg/shared/orac/data/predef_lsm/'
-                    #'MSG_000E_LSM.nc',  # SEVIRI 0 degrees
-                    'MSG_415E_LSM.nc',  # SEVIRI 41.5 degrees
+                    'MSG_000E_LSM.nc',  # SEVIRI 0 degrees
+                    #'MSG_415E_LSM.nc',  # SEVIRI 41.5 degrees
                     #'AHI_141E_LSM.nc',  # AHI
     'pregeo_file' : '/home/prataa/data/predef_geo/'#'/network/aopp/matin/eodg/shared/orac/data/predef_lsm/'
                     #'MSG_000E_ANGLES.v01.nc',  # SEVIRI 0 degrees
-                    'MSG_415E_ANGLES.v02.nc',  # SEVIRI 41.5 degrees
+                    'MSG_000E_ANGLES.v02.nc',  # SEVIRI 0 degrees
+                    #'MSG_415E_ANGLES.v02.nc',  # SEVIRI 41.5 degrees
                     #'AHI_141E_ANGLES.nc',  # AHI
     # Climatology of Swansea s and p parameters
     'swansea_dir' : '/network/aopp/matin/eodg/atsr/swansea_cb'
